29df76a7409b187053e26870e45fbe3fb485f598
mclaramarinho <marinho.claramb@gmail.com>
2024-02-16 11:09:01 -0300

    CORS

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def shutdown_db_client():
 
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["*", "http://localhost:5173/"],
+                   allow_origins=["*"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"],
